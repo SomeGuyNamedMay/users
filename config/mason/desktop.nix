5b24242a3397b88a56f9433de10714a0106f1547
SomeGuyNamedMy <mfdear444@gmail.com>
2023-05-13 17:07:40 -0400

    remove firefox

--- config/mason/desktop.nix
+++ config/mason/desktop.nix
@@ -182,16 +182,16 @@
   xdg.mimeApps = {
     enable = true;
     associations.added = {
-        "x-scheme-handler/http" = "firefox.desktop";
-        "x-scheme-handler/https" = "firefox.desktop";
-        "x-scheme-handler/chrome" = "firefox.desktop";
-        "text/html" = "firefox.desktop";
-        "application/x-extension-htm" = "firefox.desktop";
-        "application/x-extension-html" = "firefox.desktop";
-        "application/x-extension-shtml" = "firefox.desktop";
-        "application/xhtml+xml" = "firefox.desktop";
-        "application/x-extension-xhtml" = "firefox.desktop";
-        "application/x-extension-xht" = "firefox.desktop";
+        "x-scheme-handler/http" = "qutebrowser.desktop";
+        "x-scheme-handler/https" = "qutebrowser.desktop";
+        "x-scheme-handler/chrome" = "qutebrowser.desktop";
+        "text/html" = "qutebrowser.desktop";
+        "application/x-extension-htm" = "qutebrowser.desktop";
+        "application/x-extension-html" = "qutebrowser.desktop";
+        "application/x-extension-shtml" = "qutebrowser.desktop";
+        "application/xhtml+xml" = "qutebrowser.desktop";
+        "application/x-extension-xhtml" = "qutebrowser.desktop";
+        "application/x-extension-xht" = "qutebrowser.desktop";
     };
     defaultApplications = {
       "image/png" = [ "imv.desktop" ];
@@ -200,16 +200,16 @@
       "application/pdf" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
       "text/plain" = [ "emacs.desktop" ];
       "inode/directory" = [ "thunar.desktop" ];
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
-      "text/html" = [ "firefox.desktop" ];
-      "application/x-extension-htm" = [ "firefox.desktop" ];
-      "application/x-extension-html" = ["firefox.desktop"];
-      "application/x-extension-shtml" = ["firefox.desktop"];
-      "application/xhtml+xml" = ["firefox.desktop"];
-      "application/x-extension-xhtml" = ["firefox.desktop"];
-      "application/x-extension-xht" = ["firefox.desktop"];
+      "x-scheme-handler/http" = [ "qutebrowser.desktop" ];
+      "x-scheme-handler/https" = [ "qutebrowser.desktop" ];
+      "x-scheme-handler/chrome" = [ "qutebrowser.desktop" ];
+      "text/html" = [ "qutebrowser.desktop" ];
+      "application/x-extension-htm" = [ "qutebrowser.desktop" ];
+      "application/x-extension-html" = ["qutebrowser.desktop"];
+      "application/x-extension-shtml" = ["qutebrowser.desktop"];
+      "application/xhtml+xml" = ["qutebrowser.desktop"];
+      "application/x-extension-xhtml" = ["qutebrowser.desktop"];
+      "application/x-extension-xht" = ["qutebrowser.desktop"];
     };
   };
 
